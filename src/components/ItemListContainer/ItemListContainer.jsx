563d6862a9436d29813bea0e38b3a9949bcf360e
José Lautaro Daniel Montaña <67487561+joselautaro@users.noreply.github.com>
2021-10-07 01:23:44 -0300

    Cambio de estilos en el menu

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -3,10 +3,8 @@ import "./ItemListContainer.css";
 
 export const ItemListContainer = ({greeting}) => {
     return (
-        <div>
             <section>
                 <h1>{greeting}</h1>
             </section>
-        </div>
     )
 }
