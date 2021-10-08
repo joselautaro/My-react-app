c0868a7f9125d5bfa139e4b0c3ee8711b92ffda1
José Lautaro Daniel Montaña <67487561+joselautaro@users.noreply.github.com>
2021-10-08 01:40:08 -0300

    array de objetos de productos

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,6 +1,36 @@
 import React from 'react'
 import "./ItemListContainer.css";
 
+
+
+const stock = [
+    {
+        id: 1,
+        name: "producto 1",
+        description: "lorem ipsum",
+        price: 150,
+        img: "https://via.placeholder.com/150",
+
+    },
+    {
+        id: 1,
+        name: "producto 2",
+        description: "lorem ipsum",
+        price: 200,
+        img: "https://via.placeholder.com/150",
+
+    },
+    {
+        id: 1,
+        name: "producto 3",
+        description: "lorem ipsum",
+        price: 300,
+        img: "https://via.placeholder.com/150",
+
+    },
+]
+
+
 export const ItemListContainer = ({greeting}) => {
     return (
             <section>
